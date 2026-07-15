145810c969bd7a8e5cf1fbde5d96b05edf3052a6
SricharanKrish <sricharan73@gmail.com>
2026-07-15 16:44:27 +0530

    Done with VS CO

--- Day8_Strings.java
+++ Day8_Strings.java
@@ -1,3 +1,27 @@
 public class Day8_Strings {
     
+    public static void main(String[] args) {
+        
+        System.out.println("To find the length of a String");
+        String s = "Welcome, Sri";
+
+        System.out.println(s.length());
+
+        System.out.println("\nTo concat / join 2 Strings");
+
+        String a = "Hello, my name";
+        String b =" is John Locke";
+
+        System.out.println(a.concat(b));
+
+        System.out.println("\nRemove spaces in between Strings");
+
+        String z = "      Hello    ";
+        System.out.println("Before removin spaces " + z);
+
+        String z1 = z.trim();
+        System.out.println("After removing spaces " + z1);
+
+        
+    }
 }
