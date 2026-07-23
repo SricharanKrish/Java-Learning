e7181702d40ccfbd8a35db24feb9d7647eb9835a
SricharanKrish <sricharan73@gmail.com>
2026-07-23 18:41:15 +0530

    July 23 started practice

---
+++ Practice_Day01.java
@@ -0,0 +1,12 @@
+
+public class Practice_Day01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Mike.\nI am learning Java.\nThis is my first program.");
+	
+		System.out.println("My name is Mike.");
+		System.out.println("I am learning Java.");
+		System.out.println("This is my first program.");
+	}
+}
